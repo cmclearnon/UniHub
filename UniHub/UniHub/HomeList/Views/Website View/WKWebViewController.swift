d79b3e0f28085564742632f11e31b723b4da2231
Chris McLearnon <chrismclearnon@icloud.com>
2020-10-15 13:22:21 +0100

    WKWebViewController activity indicator

--- UniHub/UniHub/HomeList/Views/Website View/WKWebViewController.swift
+++ UniHub/UniHub/HomeList/Views/Website View/WKWebViewController.swift
@@ -11,11 +11,18 @@ import WebKit
 class WKWebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     
     private var webPage: String
+    private var webViewIsLoading: Bool = false
+    private var refreshButtonShouldHide: Bool = false
     fileprivate var webView: WKWebView
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupActivityIndicatorNavigationBarButtonItem()
         self.webView.load(self.webPage)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     init(withWebPage webPageString: String) {
@@ -23,26 +30,61 @@ class WKWebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate 
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         super.init(nibName: nil, bundle: nil)
-        view.backgroundColor = UIColor.systemBackground
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    @objc
+    func refreshWebView(_ sender: UIBarButtonItem) {
+        webView.reload()
+    }
+    
+    func setupRefreshNavigationBarButtonItem() {
+        let rightButton = UIBarButtonItem(title: "Refresh", style: .plain, target: self, action: #selector(refreshWebView(_:)))
+        self.navigationItem.rightBarButtonItem = rightButton
+    }
+    
+    func setupActivityIndicatorNavigationBarButtonItem() {
+        let indicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
+        indicator.hidesWhenStopped = true
+        indicator.startAnimating()
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: indicator)
+    }
+
     override func loadView() {
         webView.uiDelegate = self
         webView.navigationDelegate = self
         webView.allowsBackForwardNavigationGestures = true
+        webView.translatesAutoresizingMaskIntoConstraints = false
         self.view = webView
+        self.edgesForExtendedLayout = []
+        self.navigationController?.navigationBar.prefersLargeTitles = false
     }
+    
+    func setupViewConstraints() {
+        view.addSubview(webView)
+        webView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        webView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        webView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
     
     func getWebView() -> WKWebView {
         return webView
     }
     
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        webViewIsLoading = true
+        setupActivityIndicatorNavigationBarButtonItem()
+    }
+    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print("Did finish nagivating to: \(webPage)")
+        webViewIsLoading = false
+        setupRefreshNavigationBarButtonItem()
     }
     
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
