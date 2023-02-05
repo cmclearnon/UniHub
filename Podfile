bcc98168ff58a149530413d8efe0e3ec21386bab
Chris McLearnon <chrismclearnon@icloud.com>
2023-02-05 13:23:14 +0000

    Install RxSwift

---
+++ Podfile
@@ -0,0 +1,20 @@
+# platform :ios, '9.0'
+
+target 'UniHub' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'RxSwift', '6.5.0'
+  pod 'RxCocoa', '6.5.0'
+
+  target 'UniHubTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'UniHubUITests' do
+    pod 'RxBlocking', '6.5.0'
+    pod 'RxTest', '6.5.0'
+  end
+
+end
---
+++ UniHub/HomeList/View Models/UniversitiesListViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  UniversitiesListViewModel.swift
+//  UniHub
+//
+//  Created by Chris McLearnon on 10/10/2020.
+//
+
+import Foundation
+import Combine
+import RxSwift
+
+class UniversitiesListViewModel: ObservableObject, Identifiable {
+    var universityList: [University]?
+    private let client = APIClient.sharedInstance()
+    
+    let onChange: (([University]) -> Void)
+
+    init(onChange: @escaping (([University]) -> Void)) {
+        self.onChange = onChange
+    }
+
+    func fetchUniversities() {
+        client.fetchUniversities(completionHandler: { [weak self] res in
+            guard let self = self else { return }
+            switch res {
+            case let .success(universities):
+                self.universityList = universities
+                self.onChange(self.universityList ?? [])
+            case let .failure(_):
+                self.universityList = []
+                self.onChange(self.universityList ?? [])
+            }
+        })
+    }
+    
+    func getViewModelListCount() -> Int {
+        guard let list = universityList else {
+            return 0
+        }
+        return list.count
+    }
+}
